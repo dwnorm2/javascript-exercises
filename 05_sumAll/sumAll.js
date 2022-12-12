838922e2131bf8476cf0e0be4e7406164da95657
dwnorm2 <dwnorm2@gmail.com>
2022-12-12 17:03:16 -0600

    Complete 04/05/06/07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  if (
+    Math.sign(num1) === -1 ||
+    Math.sign(num2) === -1 ||
+    typeof num1 !== "number" ||
+    typeof num2 !== "number"
+  ) {
+    return "ERROR";
+  } else if (num1 > num2) {
+    for (i = num2; i <= num1; i++) {
+      sum += i;
+    }
+    return sum;
+  } else
+    for (i = num1; i <= num2; i++) {
+      sum += i;
+    }
+  return sum;
 };
 
+// npm test sumAll.spec.js
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fTemp) {
+  let fToCel = ((fTemp - 32) * 5) / 9;
+  return Math.round(fToCel * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cTemp) {
+  let cToFahr = (cTemp * 9) / 5 + 32;
+  return Math.round(cToFahr * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
